0f85a12b4b5456be185c51b145b86a68494d15ad
Tomás Almeida <tomas.hugo@gmail.com>
2023-05-28 23:57:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='card'>
+        <img src="minha_foto.jpg" alt="minha foto" width={317} height={317} className='profilePhoto' />
+      </div>
+    </div>
+  )
+}
+
+export default App
